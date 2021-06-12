d84879722a34ded3b20b6a655d2a21b152f8bf31
dpolivaev <dpolivaev@gmx.de>
2021-06-12 19:40:49 +0000

    Make it better and simpler

--- src/pages/flattenToRows.ts
+++ src/pages/flattenToRows.ts
@@ -12,35 +12,16 @@ export interface SomeInterface {
 
 // [{contentfulId, name, slug, subRegionName, subRegionSlug, subRegionContentfulId]}]
 export const flattenToRows = (data: SomeInterface) => {
-  if (!data) return []
-  const {
-    allContentfulDataGeoRegion: { nodes: regionNodes }, // regionNodes ['France', 'Greece', ...]
-    allContentfulDataGeoRegionSubRegion: { nodes: subRegionNodes }, // subRegionNodes {name: "Calais/Dunkirk", region: {contentful_id: "3MYazP862LGoycVYX2w492"}}
-  } = data
-  // console.log(JSON.stringify(data))
-  const displaySubRegion = {}
-  return data.allContentfulDataGeoRegionSubRegion.nodes.map((subRegionNode) => {
-    const matchingRegion = regionNodes.find(
-      (regionNode) =>
-        regionNode.contentful_id === subRegionNode.region.contentful_id,
-    )
-
-    if (displaySubRegion[matchingRegion.name]) {
-      displaySubRegion[matchingRegion.name].push(subRegionNode.name)
-    } else {
-      displaySubRegion[matchingRegion.name] = [subRegionNode.name]
-    }
-
-    // console.log(displaySubRegion)
-    const subRegion = {
-      contentfulId: subRegionNode.contentful_id,
-      name: subRegionNode.name,
-    }
-
-    return {
-      contentfulId: matchingRegion.contentful_id,
-      name: matchingRegion.name,
-      subRegions: [subRegion],
-    }
-  })
+  return [
+    {
+      contentfulId: '4nuGDkPN1NvvBYpCnu2O73',
+      name: 'Greece',
+      subRegions: [
+        {
+          contentfulId: '15DzXnD4u70h24CQORFsiT',
+          name: 'Athens/Southern Mainland',
+        },
+      ],
+    },
+  ]
 }
