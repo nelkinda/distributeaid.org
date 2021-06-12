fb4b7a9839e6e5fdd153fec4e57f30168ed37c58
dpolivaev <dpolivaev@gmx.de>
2021-06-12 19:50:24 +0000

    Remove dead code

--- src/pages/flattenToRows.ts
+++ src/pages/flattenToRows.ts
@@ -25,16 +25,4 @@ export const flattenToRows = (data: SomeInterface) => {
     }
   })
   return regions
-  return [
-    {
-      contentfulId: '4nuGDkPN1NvvBYpCnu2O73',
-      name: 'Greece',
-      subRegions: [
-        {
-          contentfulId: '15DzXnD4u70h24CQORFsiT',
-          name: 'Athens/Southern Mainland',
-        },
-      ],
-    },
-  ]
 }
